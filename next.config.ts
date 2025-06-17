9459df0692cce53d32351b6a2d68112f81ac70be
PDFixy <PDFixy.com@gmail.com>
2025-06-16 18:35:10 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,67 +1,7 @@
-import type { NextConfig } from 'next';
-import withBundleAnalyzer from '@next/bundle-analyzer';
-import { withSentryConfig } from '@sentry/nextjs';
-import createNextIntlPlugin from 'next-intl/plugin';
-
-// Define the base Next.js configuration
-const baseConfig: NextConfig = {
-  eslint: {
-    dirs: ['.'],
-  },
-  poweredByHeader: false,
-  reactStrictMode: true,
-  serverExternalPackages: ['@electric-sql/pglite'],
-};
-
-// Initialize the Next-Intl plugin
-let configWithPlugins = createNextIntlPlugin('./src/libs/I18n.ts')(baseConfig);
-
-// Conditionally enable bundle analysis
-if (process.env.ANALYZE === 'true') {
-  configWithPlugins = withBundleAnalyzer()(configWithPlugins);
-}
-
-// Conditionally enable Sentry configuration
-if (!process.env.NEXT_PUBLIC_SENTRY_DISABLED) {
-  configWithPlugins = withSentryConfig(configWithPlugins, {
-    // For all available options, see:
-    // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-    // FIXME: Add your Sentry organization and project names
-    org: 'nextjs-boilerplate-org',
-    project: 'nextjs-boilerplate',
-
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
-
-    // For all available options, see:
-    
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    reactComponentAnnotation: {
-      enabled: true,
-    },
-
-    
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: '/monitoring',
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Disable Sentry telemetry
-    telemetry: false,
-  });
-}
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-export default nextConfig
+  telemetry: false,
+};
 
 export default nextConfig;
