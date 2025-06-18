6c02339a901fceb1781689b6d551b7b0fec0b971
PDFixy <PDFixy.com@gmail.com>
2025-06-17 20:09:28 -0700

    Update team.tsx

--- pages/team.tsx
+++ pages/team.tsx
@@ -1,1 +1,17 @@
+import Layout from '../components/Layout';
+import MeetTheTeam from '../components/MeetTheTeam';
 
+export default function TeamPage() {
+  return (
+    <Layout>
+      <section className="bg-white py-16 px-6">
+        <div className="max-w-7xl mx-auto">
+          <h1 className="text-4xl font-bold mb-6 text-center text-gray-800">
+            Meet the Entire PD Plumbing Team
+          </h1>
+          <MeetTheTeam />
+        </div>
+      </section>
+    </Layout>
+  );
+}
