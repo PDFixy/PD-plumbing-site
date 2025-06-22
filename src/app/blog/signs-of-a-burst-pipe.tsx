96e51da4463d731a5b1ed92da8c12d95905356ec
PDFixy <PDFixy.com@gmail.com>
2025-06-22 16:00:40 -0700

    Delete src/app/blog directory

--- src/app/blog/signs-of-a-burst-pipe.tsx
+++
@@ -1,8 +0,0 @@
-export default function Blog() {
-  return (
-    <div>
-      <h1>Signs Of A Burst Pipe</h1>
-      <p>Fixy&apos;s tips and tricks to keep your home safe. Don&apos;t miss out on plumbing peace of mind!</p>
-    </div>
-  );
-}
