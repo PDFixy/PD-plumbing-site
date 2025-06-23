0a907e8b4fd96494fdba17c84898f79263bcc47e
PDFixy <PDFixy.com@gmail.com>
2025-06-23 09:11:30 -0700

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,27 @@
 "use client";
 
-import Header from "./Header";
-import Footer from "./Footer";
-import CookieBanner from "./CookieBanner";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import CookieBanner from "@/components/CookieBanner";
+import MobileStickyCTA from "@/components/MobileStickyCTA";
+import GoogleReviewBadge from "@/components/GoogleReviewBadge";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen bg-white text-gray-800">
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow">
+        {children}
+
+        {/* Google Rating Badge (visible on all pages just before footer) */}
+        <div className="flex justify-center mt-12">
+          <GoogleReviewBadge />
+        </div>
+      </main>
+
+      {/* Mobile sticky call/message bar */}
+      <MobileStickyCTA />
+
       <CookieBanner />
       <Footer />
     </div>
