23f54609f9e7d1f9d10ba5d121301b7af378444d
PDFixy <PDFixy.com@gmail.com>
2025-06-22 20:07:17 -0700

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-white text-gray-800">
       <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
