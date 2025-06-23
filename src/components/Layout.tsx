ca30bc765d86e3db178aa11126cf972aafd48cd3
PDFixy <PDFixy.com@gmail.com>
2025-06-23 04:22:24 -0700

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,15 @@
-'use client';
+"use client";
 
-import Header from './Header';
-import Footer from './Footer';
+import Header from "./Header";
+import Footer from "./Footer";
+import CookieBanner from "./CookieBanner";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen bg-white text-gray-800">
       <Header />
       <main className="flex-grow">{children}</main>
+      <CookieBanner />
       <Footer />
     </div>
   );
