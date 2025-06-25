df430cfc35ac62d32c757794130be664a861a064
Colby Moore <pdfixy.com@gmail.com>
2025-06-24 18:10:30 -0700

    update button

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <button
+        className={cn(
+          "px-4 py-2 bg-blue-600 text-white font-semibold rounded-xl hover:bg-blue-700 transition",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Button.displayName = "Button";
---
+++ src/components/ui/card.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export function Card({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("rounded-2xl border border-gray-200 bg-white shadow p-6", className)}
+      {...props}
+    />
+  );
+}
+
+export function CardContent({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("p-2", className)} {...props} />;
+}
