0adb5a9c7843c6788e7631b03a0a9c535c103601
Colby Moore <pdfixy.com@gmail.com>
2025-06-24 20:16:00 -0700

    Switch to .eslintrc.js for Vercel compatibility

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+﻿/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ["next/core-web-vitals", "next"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-empty-object-type": "off"
+  }
+};
